e6ddb071c260052f3f0a8dfc7cd4fd40e023e29b
Rishab Garg <me@rishab-garg.me>
2025-03-06 03:16:29 +0100

    death animation

--- scripts/win/win.gd
+++ scripts/win/win.gd
@@ -1,8 +1,16 @@
 extends Area2D
 
+@onready var timer = $Timer
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		print("win !!")
-		var level : Level = body.get_tree().current_scene
-		print(level.next_level)
-		get_tree().change_scene_to_file(level.next_level)
+		Engine.time_scale = 0.5
+		body.get_node("CollisionShape2D").queue_free()
+		timer.start()
+
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	var level : Level = get_tree().current_scene
+	get_tree().change_scene_to_file(level.next_level)
