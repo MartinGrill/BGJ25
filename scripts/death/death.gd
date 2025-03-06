6f0d1a545a79f58f6a7e9eb263b1043d2de43d8f
Rishab Garg <me@rishab-garg.me>
2025-03-06 17:01:23 +0100

    code

--- scripts/death/death.gd
+++ scripts/death/death.gd
@@ -5,6 +5,7 @@ extends Area2D
 @onready var animation_player = $CanvasLayer/AnimationPlayer
 
 @export var is_lava: bool
+
 var dead := false
 
 func _ready() -> void:
@@ -14,18 +15,13 @@ func _process(delta: float) -> void:
 	if dead: return
 	
 	for body in self.get_overlapping_bodies():
-		if body is not Player: return
-		
-		var player: Player = body
-	
-		if is_lava and player.current_element == Player.Element.Fire:
-			print("passiv !!")
-			return
+		if body is not Player or (is_lava and body.current_element == Player.Element.Fire): return
 		
 		Engine.time_scale = 0.5
 		color_rect.visible = true
 		animation_player.play("death/fade_to_black")
 		dead = true
+		
 		timer.start()
 		
 
