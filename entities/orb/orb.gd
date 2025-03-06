f8f9ecf2dc712a14b6cde4aba8f32155208f14e9
Rishab Garg <me@rishab-garg.me>
2025-03-06 20:09:35 +0100

    orb??

--- entities/orb/orb.gd
+++ entities/orb/orb.gd
@@ -3,7 +3,21 @@ extends Area2D
 enum OrbElement { Universal, Water, Fire, Air, Earth }
 @export var element: OrbElement
 
+@onready var water = $Water
+@onready var fire = $Fire
+@onready var air = $Air
+@onready var earth = $Earth
+@onready var universal = $Universal
+
 signal add_orb(element: OrbElement)
+
+func _ready() -> void:	
+	match element:
+		OrbElement.Universal: universal.visible = true
+		OrbElement.Water: water.visible = true
+		OrbElement.Fire: fire.visible = true
+		OrbElement.Air: air.visible = true
+		OrbElement.Earth: earth.visible = true
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is not Player: return
