875bbc0f0bbdbd9460647e4cb56c22f10be233f2
Rishab Garg <me@rishab-garg.me>
2025-03-06 06:13:53 +0100

    lava passiv

--- scripts/death/death.gd
+++ scripts/death/death.gd
@@ -4,16 +4,23 @@ extends Area2D
 @onready var color_rect = $CanvasLayer/ColorRect
 @onready var animation_player = $CanvasLayer/AnimationPlayer
 
+@export var is_lava: bool
+
 func _ready() -> void:
 	color_rect.visible = false
 
 # Signal that player enters death zone
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
+		var player: Player = body
+		
+		if is_lava and player.current_element == Player.Element.Fire:
+			print("passiv !!")
+			return
+		
 		Engine.time_scale = 0.5
 		color_rect.visible = true
 		animation_player.play("death/fade_to_black")
-		body.get_node("CollisionShape2D").queue_free()
 		timer.start()
 
 func _on_timer_timeout() -> void:
